ee527e54d6d92cfa7405a19d49842e69a827515f
Ocelote <220468925+owcer@users.noreply.github.com>
2025-12-28 12:59:23 +0700

    Update bcf.js
    instructions

--- bcf.js
+++ bcf.js
@@ -1,13 +1,22 @@
 /*
-1.put the world code shown in bloxd code
-2.put the code block shown in a code block for test
-3.type /log until all messages have come, and no more are coming
-4.edit it to match your needs once you get it working
+bcf - bloxd chat faker
+Objective: to show the triviality and give everyone
+the power to easily create fake bloxd chat screenshots.
+
+Educational Literacy. Watermarks never ever.
+Licensable under CC0. No credit required.
+You can even steal and sell my code with or without modification as your own and I would not care.
+Furthermore, I don't care what purposes you use this for.
+1.create a flat test world
+2.put the world code shown in bloxd code
+3.put the code block shown in a code block for test
+4.type /log until all messages have come, and no more are coming
+5.edit it to match your needs once you get it working
 */
-//World code:
+//=World code:=
 bd=[],bcf=(e,t)=>{let f=["#dff8ff","#ffff66","#b3ff66","#66ff66","#66ffb3","#66ffff","#66bcff","#cc66ff","#ff80bb","#ff6666","#ffaa66"],s=t.trim().split("\n");for(let e of s)if(e.includes(":")&&!e.startsWith("Tip")){let t=[],s=e.indexOf(":"),r=e.substring(0,s+1),l=e.substring(s+1),o="",i="#dff8ff";if(r.startsWith("[")){let e=r.indexOf("]"),f=r.substring(0,e+1);f.includes("Youtube")||f.includes("Super")||f.includes("Dev")||(o=f,t.push({str:o,style:{color:"#2eeb82"}}),r=r.slice(o.length))}if(r.includes("[Youtube]")){let e=r.indexOf("[Youtube]");e>0&&t.push({str:r.substring(0,e),style:{color:"#dff8ff"}}),t.push({str:"[",style:{color:"#fff"}},{icon:"youtube",style:{color:"#fe0000"}},{str:" You",style:{color:"#fff"}},{str:"Tube",style:{color:"#fe0000"}},{str:"]",style:{color:"#fff"}}),r=r.slice(e+9)}if(r.includes("[Dev]")){let e=r.indexOf("[Dev]");e>0&&t.push({str:r.substring(0,e),style:{color:"#dff8ff"}}),t.push({str:"[",style:{color:"#cef3ff"}},{icon:"wrench",style:{color:"#a4a4a4"}},{str:" Dev]",style:{color:"#cef3ff"}}),r=r.slice(e+5)}let c=r.match(/\[(\d+)\s?Super\]/);if(c){let e=c[0],s=parseInt(c[1]),l=r.indexOf(e);l>0&&t.push({str:r.substring(0,l),style:{color:"#dff8ff"}}),t.push({str:"[",style:{color:"#fc0"}},{icon:"zap",style:{color:"#fc0"}},{str:" Super]",style:{color:"#fc0"}}),i=f[s]||"#dff8ff",r=r.slice(l+e.length)}t.push({str:r,style:{color:i}});let n="[Tribe]"===o?"#2eeb82":"#ffffff";t.push({str:l,style:{color:n}}),bd.push(t)}else{let t="#cef3ff";(e.endsWith(" has joined the tribe")||"The chunk is now yours. Other players cannot build here."===e||"Protectors protect the chunk they are in, with 32 cubes on each side, as well as the chunk above and below."===e)&&(t="#2eeb82"),(e.endsWith(" has left the tribe")||e.endsWith(" has been removed from the tribe")||e.startsWith("Left tribe ")||e.startsWith("Removed ")&&e.endsWith(" from the tribe"))&&(t="#ff9d87"),bd.push([{str:e,style:{color:t}}])}},playerCommand=(e,t)=>{if("log"===t)for(let t=0;bd.length&&t<16;t++)api.sendMessage(e,bd.shift());return!0};
-// end of world code
-//Code block: 
+//=end of world code=
+//=Code block:=
 bcf(myId,`
 coolperson joined
 someone joined
@@ -47,4 +56,4 @@ Arthur left
 [cat] [Youtuber] [9Super] coolperson: test
 [cat] [Youtuber] [9Super] coolperson: nice
 `)
-//end of code block
+//=end of code block=
